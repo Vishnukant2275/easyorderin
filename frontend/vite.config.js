abb16aeeb458513e291065657b41c124c4eab8d5
Vishnukant2275 <vishnukant2275@gmail.com>
2025-11-20 20:02:59 +0530

    still changing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // your backend URL
+        target: "https://easyorderin.onrender.com", // your backend URL
         changeOrigin: true,
       },
     },
